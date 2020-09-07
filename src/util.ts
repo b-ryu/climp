6f8374f26b3bacb703962326804f1d827c99e473
Brian Ryu <46533681+b-ryu@users.noreply.github.com>
2020-09-07 07:33:16 -0400

    Extract utils

---
+++ src/util.ts
@@ -0,0 +1,44 @@
+import type {Arg, Type} from './types';
+
+export function stripArgName(argName: string) {
+  return argName.substring(2);
+}
+
+// TODO make this prefix configurable
+export function isArgName(arg: string) {
+  return arg.startsWith('--');
+}
+
+export function argType(arg: Arg) {
+  if ('type' in arg) {
+    return arg.type === 'boolean' ? 'boolean' : 'singular';
+  } else {
+    return Array.isArray(arg.types) ? 'finite' : 'infinite';
+  }
+}
+
+// Returns null if argValue cannot be properly casted
+export function castArgValue(argValue: string, type: Type) {
+  if (type === 'boolean' || type === 'cast') {
+    switch (argValue.toLowerCase()) {
+      case 'true':
+        return true;
+      case 'false':
+        return false;
+      default:
+        if (type === 'boolean') return null;
+    }
+  }
+
+  if (type === 'number' || type === 'cast') {
+    const numberValue = Number(argValue);
+
+    if (isNaN(numberValue)) {
+      if (type === 'number') return null;
+    } else {
+      return numberValue;
+    }
+  }
+
+  return argValue;
+}
