66e7ef5b449580c70e7ba7e4cfe19e8ea9bb8f5a
Brian Ryu <46533681+b-ryu@users.noreply.github.com>
2020-09-09 20:51:45 -0400

    Fix bug from TS Error class extensions

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,14 @@
+/*
+  lmao wtf: https://github.com/microsoft/TypeScript/issues/13965
+*/
 export default class ClimpError extends Error {
+  __proto__: Error;
+
   constructor({message}) {
+    const proto = new.target.prototype;
     super(message);
 
     this.name = 'ClimpError';
+    this.__proto__ = proto;
   }
 }
